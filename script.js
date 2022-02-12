9375eca9e691ea15b70124ec38de640d305d26da
SSAtGitHUB <99533895+SSAtGitHUB@users.noreply.github.com>
2022-02-12 13:55:22 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function chngthma(){
+  document.body.style.background = "linear-gradient(to bottom, #2980b9, #6dd5fa, #ffffff)"
+  document.getElementById("nam").style.color = "white"
+  document.getElementById("nam").style.border = "4px solid yellow"
+  document.getElementById("hehe").style.color = "yellow"
+}
+function chngthm(){
+  document.body.style.background = "beige"
+  document.getElementById("nam").style.color = "black"
+  document.getElementById("nam").style.border = "4px solid blueviolet"
+  document.getElementById("hehe").style.color = "black"
+}
